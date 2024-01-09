d8b7efd02aec181cb2b367bc3acb529fd02613b0
Frankie <frankie@gmail.com>
2024-01-09 17:41:19 +0700

    Migrate auto book model

--- migration/migration.go
+++ migration/migration.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"frankie060392/rest-api-clean-arch/bootstrap"
-	"frankie060392/rest-api-clean-arch/internal/user/model"
+	bookModel "frankie060392/rest-api-clean-arch/internal/book/model"
+	userModel "frankie060392/rest-api-clean-arch/internal/user/model"
 )
 
 func main() {
 	config := bootstrap.LoadConfig(".")
 	DB := bootstrap.NewDbConnection(config)
-	DB.AutoMigrate(&model.User{})
+	DB.AutoMigrate(&bookModel.Book{})
+	DB.AutoMigrate(&userModel.User{})
 	fmt.Println("? Migration complete")
 }
