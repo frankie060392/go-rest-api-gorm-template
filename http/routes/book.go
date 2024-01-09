ab8f7644750ee3470902bc0524c96fb2748958f0
Frankie <frankie@gmail.com>
2024-01-09 17:22:32 +0700

    Add book route

---
+++ http/routes/book.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"frankie060392/rest-api-clean-arch/http/handlers"
+	"frankie060392/rest-api-clean-arch/internal/book/repository"
+	"frankie060392/rest-api-clean-arch/internal/book/service"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func NewBookRouter(db *gorm.DB, group *gin.RouterGroup) {
+	router := group.Group("book")
+	ur := repository.NewBookRepository(db)
+	us := service.NewBookService(ur)
+	uh := handlers.NewBookHandler(us)
+	router.POST("/", uh.Create)
+}
