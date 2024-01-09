a631042dd529e6a7b9ebe0d7750bdb3f72e5dc1e
Frankie <frankie@gmail.com>
2024-01-09 11:45:19 +0700

    Add http handler(controller)

---
+++ http/handlers/user.go
@@ -0,0 +1,60 @@
+package handlers
+
+import (
+	"frankie060392/rest-api-clean-arch/internal/user/model"
+	"frankie060392/rest-api-clean-arch/pkg/utils"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	userService model.UserService
+}
+
+func (uh *UserHandler) Create(ctx *gin.Context) {
+	var payload *model.SignUpInput
+	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	if payload.Password != payload.PasswordConfirm {
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	hashedPassword, err := utils.HashPassword(payload.Password)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	now := time.Now()
+	newUser := model.User{
+		Name:      payload.Name,
+		Email:     strings.ToLower(payload.Email),
+		Password:  hashedPassword,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+	err = uh.userService.CreateUser(ctx, &newUser)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, nil)
+		return
+	}
+
+	userResponse := &model.UserResponse{
+		ID:        newUser.ID,
+		Name:      newUser.Name,
+		Email:     newUser.Email,
+		CreatedAt: newUser.CreatedAt,
+		UpdatedAt: newUser.UpdatedAt,
+	}
+	ctx.JSON(http.StatusCreated, userResponse)
+}
+
+func (uh *UserHandler) GetById(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, nil)
+}
